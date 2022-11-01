3e30a8eae9bf1f7b8204460948be2ff45dcbbb16
Elena Lemeskina <gogulinaelena@yandex.ru>
2022-11-01 13:04:48 +0300

    Editing maxSize

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -5,11 +5,11 @@ import java.util.stream.Collectors;
 
 public class Todos {
     protected List<String> taskList = new ArrayList<>();
-    protected final int MaxSize = 7;
+    protected final int maxSize = 7;
 
 
     public void addTask(String task) {
-        if (taskList.size() < MaxSize) {
+        if (taskList.size() < maxSize) {
             if (!taskList.contains(task)) {
                 taskList.add(task);
             }
